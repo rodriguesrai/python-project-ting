737019a2e0084b1345635211b9f3640dc709da77
rodriguesrai <rairodrigues_sf@hotmail.com>
2024-03-08 15:09:01 -0300

    req8

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -22,4 +22,23 @@ def exists_word(word, instance):
 
 
 def search_by_word(word, instance):
-    """Aqui irá sua implementação"""
+    result = []
+
+    for item in instance._queue:
+        file_name = item["nome_do_arquivo"]
+        occurrences = []
+
+        for i, line_content in enumerate(item["linhas_do_arquivo"], start=1):
+            if word.lower() in line_content.lower():
+                occurrences.append({"linha": i, "conteudo": line_content})
+
+        if occurrences:
+            result.append(
+                {
+                    "palavra": word,
+                    "arquivo": file_name,
+                    "ocorrencias": occurrences,
+                }
+            )
+
+    return result
