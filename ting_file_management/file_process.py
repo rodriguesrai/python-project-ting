c6be60570e6d746b782c78ebc6369821038ff9b0
rodriguesrai <rairodrigues_sf@hotmail.com>
2024-03-08 13:50:57 -0300

    req3: implementa process

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,14 +1,18 @@
 from ting_file_management.file_management import txt_importer
-import os
 import sys
 
 
 def process(path_file, instance):
-    file_name = os.path.basename(path_file)
+    file_lines = txt_importer(path_file)
+    file_info = {
+        "nome_do_arquivo": path_file,
+        "qtd_linhas": len(file_lines),
+        "linhas_do_arquivo": file_lines,
+    }
 
-    if file_name in instance:
-        print(f"Arquivo {file_name} já importado", file=sys.stderr)
-        return
+    if file_info not in instance._queue:
+        instance.enqueue(file_info)
+        print(file_info, file=sys.stdout)
 
 
 def remove(instance):
--- ting_file_management/queue.py
+++ ting_file_management/queue.py
@@ -3,18 +3,18 @@ from ting_file_management.abstract_queue import AbstractQueue
 
 class Queue(AbstractQueue):
     def __init__(self):
-        self.__queue = []
+        self._queue = []
 
     def __len__(self):
-        return len(self.__queue)
+        return len(self._queue)
 
     def enqueue(self, value):
-        self.__queue.append(value)
+        self._queue.append(value)
 
     def dequeue(self):
-        return self.__queue.pop(0)
+        return self._queue.pop(0)
 
     def search(self, index):
-        if index < 0 or index >= len(self.__queue):
+        if index < 0 or index >= len(self._queue):
             raise IndexError("Índice Inválido ou Inexistente")
-        return self.__queue[index]
+        return self._queue[index]
