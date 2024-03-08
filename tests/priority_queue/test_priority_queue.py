71bf8bc73435f5bc222fd0421f172bb10f74aea9
rodriguesrai <rairodrigues_sf@hotmail.com>
2024-03-08 14:49:11 -0300

    req 6: implementa testes de PriorityQueue

--- tests/priority_queue/test_priority_queue.py
+++ tests/priority_queue/test_priority_queue.py
@@ -1,5 +1,22 @@
-# from ting_file_management.priority_queue import PriorityQueue
+from ting_file_management.priority_queue import PriorityQueue
+import pytest
 
 
 def test_basic_priority_queueing():
-    """Aqui irá sua implementação"""
+    PQ = PriorityQueue()
+    assert len(PQ) == 0
+
+    PQ.enqueue({"qtd_linhas": 8})
+    PQ.enqueue({"qtd_linhas": 3})
+    PQ.enqueue({"qtd_linhas": 2})
+    assert len(PQ.regular_priority) == 1
+    assert len(PQ.high_priority) == 2
+
+    PQ.dequeue()
+    assert len(PQ.high_priority) == 1
+
+    teste = PQ.search(0)
+    assert teste["qtd_linhas"] == 2
+
+    with pytest.raises(IndexError):
+        PQ.search(5)
