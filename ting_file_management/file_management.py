b428651269e969f38e129f1089d3c10c17dfb38d
rodriguesrai <rairodrigues_sf@hotmail.com>
2024-03-08 13:10:43 -0300

    fix lint

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,5 +1,6 @@
 import sys
 
+
 def txt_importer(path_file):
     try:
         if not path_file.endswith(".txt"):
--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,14 @@
+from ting_file_management.file_management import txt_importer
+import os
+import sys
+
+
 def process(path_file, instance):
-    """Aqui irá sua implementação"""
+    file_name = os.path.basename(path_file)
+
+    if file_name in instance:
+        print(f"Arquivo {file_name} já importado", file=sys.stderr)
+        return
 
 
 def remove(instance):
