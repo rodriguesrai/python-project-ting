2923057bf565e469700d571088edb0a2075a7f5f
rodriguesrai <rairodrigues_sf@hotmail.com>
2024-03-08 14:05:00 -0300

    req 4: implementa remove

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -16,7 +16,14 @@ def process(path_file, instance):
 
 
 def remove(instance):
-    """Aqui irá sua implementação"""
+    if instance.__len__() == 0:
+        print("Não há elementos", file=sys.stdout)
+    else:
+        item = instance.dequeue()
+        print(
+            f"Arquivo {item['nome_do_arquivo']} removido com sucesso",
+            file=sys.stdout,
+        )
 
 
 def file_metadata(instance, position):
